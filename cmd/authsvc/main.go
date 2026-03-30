ed6c3fc0077c510c5b1dff02f00c735b14be58ee
mohmdsaalim <muhammedsaalim005@gmail.com>
2026-03-30 09:56:07 +0530

    tested the folders 70% ok running condition

---
+++ cmd/authsvc/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mohmdsaalim/EngineX/pkg/apperr"
+)
+
+func main() {
+	a := apperr.Mbape(1, 2)
+	fmt.Println(a)
+	
+}
---
+++ pkg/apperr/errors.go
@@ -0,0 +1,5 @@
+package apperr
+
+func Mbape(a, b int) int {
+	return a + b 
+}
